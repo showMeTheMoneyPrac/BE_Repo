23c72e04f91a4d953baf8748aec7a6dce63db6ba
john <john3210of@gmail.com>
2022-05-06 10:25:40 +0900

    [test]

---
+++ src/test/java/com/shop/mall/ShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
