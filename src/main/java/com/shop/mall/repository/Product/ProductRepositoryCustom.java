92eabb018805e72eeca41fd61b08edf54fc136c0
leeseungsoo <seungsoo0701@gmail.com>
2022-05-24 02:47:20 +0900

    🚜[feat] ProductRepository QueryDsl 적용

---
+++ src/main/java/com/shop/mall/repository/Product/ProductRepositoryCustom.java
@@ -0,0 +1,13 @@
+package com.shop.mall.repository.Product;
+
+import com.shop.mall.dto.ProductResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface ProductRepositoryCustom {
+    Page<ProductResponseDto.ProductList> searchByRecent(Pageable pageable, String category, String searchKeyword);
+    Page<ProductResponseDto.ProductList> searchByCost(Pageable pageable,String category,String searchKeyword);
+    Page<ProductResponseDto.ProductList> searchByReviewCnt(Pageable pageable,String category,String searchKeyword);
+}
