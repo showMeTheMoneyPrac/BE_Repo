a48a7fe624ec9b27979709b9ff845314079c1258
leeseungsoo <seungsoo0701@gmail.com>
2022-05-23 20:59:04 +0900

    📝 [feat] 9번 API 작업 완료

--- src/main/java/com/shop/mall/controller/ProductController.java
+++ src/main/java/com/shop/mall/controller/ProductController.java
@@ -4,9 +4,11 @@ import com.shop.mall.dto.ProductResponseDto;
 import com.shop.mall.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,14 +19,22 @@ public class ProductController {
 
     private final ProductService productService;
 
+    //8번 API 메인 페이지 상품 데이터(pageable)
     @GetMapping("/products")
-    public List<ProductResponseDto.productList> findAllProduct(@PageableDefault(size = 8) Pageable pageable) {
+    public List<ProductResponseDto.ProductList> findAllProduct(@PageableDefault(size = 8) Pageable pageable) {
         return productService.productList(pageable).getContent();
     }
 
+    //9번 API 상품 상세 데이터
     @GetMapping("/products/{productId}")
-    public ProductResponseDto.productsDetail ProductDetail(@PathVariable Long productId){
+    public ProductResponseDto.ProductsDetail productDetail(@PathVariable Long productId){
         return productService.productsDetail(productId);
     }
+
+    //10번 API 상품 카테고리 및 검색
+//    @GetMapping("/products")
+//    public List<ProductResponseDto.ProductList> findConditionsProduct(@PageableDefault(size = 8, sort = "createdAt") Pageable pageable, @RequestParam(value = "sort") String sort, @RequestParam(value = "category") String category, @RequestParam(value = "searchKeyword") String searchKeyword){
+//        return productService.conditionProductList(pageable,)
+//    }
 
 }
--- src/main/java/com/shop/mall/dto/ProductResponseDto.java
+++ src/main/java/com/shop/mall/dto/ProductResponseDto.java
@@ -3,10 +3,12 @@ package com.shop.mall.dto;
 import com.shop.mall.domain.Img;
 import com.shop.mall.domain.Product;
 import com.shop.mall.domain.ProductOption;
-import com.shop.mall.domain.Review;
 import lombok.*;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static com.shop.mall.dto.ReviewResponseDto.ReviewList.reviewListFrom;
 
 public class ProductResponseDto {
 
@@ -15,7 +17,7 @@ public class ProductResponseDto {
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
-    public static class productList {
+    public static class ProductList {
         private Long productId;
         private String title;
         private String category;
@@ -24,8 +26,8 @@ public class ProductResponseDto {
         private int price;
         private String firstImg;
 
-        public static productList productListFrom(Product product){
-            return productList.builder()
+        public static ProductList productListFrom(Product product) {
+            return ProductList.builder()
                     .productId(product.getId())
                     .title(product.getTitle())
                     .category(product.getCategory())
@@ -36,31 +38,51 @@ public class ProductResponseDto {
                     .build();
         }
     }
+
     @Getter
     @Setter
     @Builder
-    public static class productsDetail{
+    public static class ProductsDetail {
         private Long productId;
         private String title;
         private String category;
         private int reviewCnt;
         private String detail;
         private int price;
-        private List<ProductOption> optionList;
-        private List<Img> imgList;
-        private List<Review> reviewList;
+        private List<String> optionList;
+        private List<String> imgList;
+        private List<ReviewResponseDto.ReviewList> reviewList;
 
-        public static productsDetail productsDetailFrom(Product product){
-            return productsDetail.builder()
+        public static ProductsDetail productsDetailFrom(Product product) {
+            List<String> optionList = new ArrayList<>();
+            List<String> imgList = new ArrayList<>();
+            List<ReviewResponseDto.ReviewList> reviewList = new ArrayList<>();
+
+            product.getProductOptionList()
+                    .stream()
+                    .map(ProductOption::getOptionContent)
+                    .forEach(optionList::add);
+
+            product.getImgList()
+                    .stream()
+                    .map(Img::getImgUrl)
+                    .forEach(imgList::add);
+
+            for (int i=0; i<product.getReviewList().size(); i++){
+                reviewList.add(reviewListFrom(product.getReviewList().get(i)));
+            }
+
+
+            return ProductsDetail.builder()
                     .productId(product.getId())
                     .title(product.getTitle())
                     .category(product.getCategory())
                     .reviewCnt(product.getReviewCnt())
                     .detail(product.getDetail().substring(1))
                     .price(product.getPrice())
-                    .optionList(product.getProductOptionList())
-                    .imgList(product.getImgList())
-                    .reviewList(product.getReviewList())
+                    .optionList(optionList)
+                    .imgList(imgList)
+                    .reviewList(reviewList)
                     .build();
         }
     }
