12fb89aa055672620b453f2e5def569102bc7782
leeseungsoo <seungsoo0701@gmail.com>
2022-05-25 20:33:37 +0900

    📰 [feat] ReviewTestCode 생성

---
+++ src/test/java/com/shop/mall/service/ReviewServiceTest.java
@@ -0,0 +1,20 @@
+package com.shop.mall.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReviewServiceTest {
+
+    @Test
+    void writeReview() {
+    }
+
+    @Test
+    void updateReview() {
+    }
+
+    @Test
+    void deleteReview() {
+    }
+}
