5b93d3ffd4cef62cf902190f0b3fd2359b3dbd23
leeseungsoo <seungsoo0701@gmail.com>
2022-05-25 15:13:10 +0900

    📰 [feat] 리뷰 작성을 위한 OrdersDetailRepository, OrdersDetailValidator 생성

---
+++ src/main/java/com/shop/mall/repository/OrdersDetailRepository.java
@@ -0,0 +1,7 @@
+package com.shop.mall.repository;
+
+import com.shop.mall.domain.OrdersDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrdersDetailRepository extends JpaRepository<OrdersDetail,Long>{
+}
