044516ffc61a0a3824cb871afd697c7adc294c60
leeseungsoo <seungsoo0701@gmail.com>
2022-05-26 00:34:53 +0900

    📰️[Feat] 15번 API 개발 완료

--- src/main/java/com/shop/mall/repository/OrdersRepository.java
+++ src/main/java/com/shop/mall/repository/OrdersRepository.java
@@ -3,5 +3,8 @@ package com.shop.mall.repository;
 import com.shop.mall.domain.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrdersRepository extends JpaRepository<Orders,Long> {
+    List<Orders> findAllByMemberId(Long memberId);
 }
