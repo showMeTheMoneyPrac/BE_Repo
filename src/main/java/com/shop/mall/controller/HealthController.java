5dcd60587ac3fd9a1c319a54113f96b87574fb94
john <john3210of@gmail.com>
2022-05-29 17:51:51 +0900

    [feat] healthController

---
+++ src/main/java/com/shop/mall/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.shop.mall.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String healthCheck(){
+        return "API test!";
+    }
+}
+
