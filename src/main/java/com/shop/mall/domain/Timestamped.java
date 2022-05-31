dc605fdbe3872232a4ee66118c3bc0d659623f6c
john <john3210of@gmail.com>
2022-05-31 21:43:36 +0900

    [fix] redis cache관련 side effect 수정

--- src/main/java/com/shop/mall/domain/Timestamped.java
+++ src/main/java/com/shop/mall/domain/Timestamped.java
@@ -1,5 +1,9 @@
 package com.shop.mall.domain;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -7,15 +11,20 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass // 맴버 변수 컬림이 되도록 함
 @EntityListeners(AuditingEntityListener.class) // 변경 되었을 대 자동 기록.
-public abstract class Timestamped {
+public abstract class Timestamped implements Serializable {
     @CreatedDate // 최초 생성 시점
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime createdAt;
 
     @LastModifiedDate // 마지막 변경 시점
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime modifiedAt;
 }
